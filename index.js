f4b3ed087350a876541c4fb8a83cad14ff51c4d4
Rachit Garg <rachitgarg56@gmail.com>
2026-06-23 22:36:53 +0530

    structured outputs

--- index.js
+++ index.js
@@ -1,36 +1,61 @@
 import {retrieveSimilarDocs} from "./retrieveSimilarDocs.js"
 import {getRagPrompt, combineDocuments} from "./utils.js"
 import {ANSWERING_MODEL} from "./constants.js"
-import { generateText } from "ai"
+import { generateText, Output } from "ai"
 import {googleGenAI} from "./config.js"
 import { ingestDocuments } from "./upsertDocuments.js"
+import { z } from 'zod';
 
-const query = "What is the cheminal reaction of photosynthesis?"
+const query = "Generate a lasagna recipe."
 
 async function main(query){
-  // split text into chunks, embed ans store into vector db
-  // await ingestDocuments(); 
+  // // split text into chunks, embed ans store into vector db
+  // // await ingestDocuments(); 
 
-  //retrieve docs that contain content relevant to the query
-  const retrievedDocs = await retrieveSimilarDocs(query)
-  // console.log(retrievedDocs)
+  // //retrieve docs that contain content relevant to the query
+  // const retrievedDocs = await retrieveSimilarDocs(query)
+  // // console.log(retrievedDocs)
 
-  //create a prompt including context docs to send to the model
+  // //create a prompt including context docs to send to the model
 
-  const contextString = combineDocuments(retrievedDocs);
+  // const contextString = combineDocuments(retrievedDocs);
 
-  //create a prompt including context docs to send to the model
-  const prompt = getRagPrompt(contextString, query)
+  // //create a prompt including context docs to send to the model
+  // const prompt = getRagPrompt(contextString, query)
 
-  // console.log(`Prompt: ${prompt}`)
+  // // console.log(`Prompt: ${prompt}`)
 
-  // //send prompt to model to generate response
-  const { text } = await generateText({
+  // // //send prompt to model to generate response
+  // const { text } = await generateText({
+  //   model: googleGenAI(ANSWERING_MODEL),
+  //   prompt: prompt
+  // });
+
+  // console.log(text);
+  basicStruturedOutput(query);
+}
+
+async function basicStruturedOutput(query) {
+  const result = await generateText({
     model: googleGenAI(ANSWERING_MODEL),
-    prompt: prompt
+    output: Output.object({
+      schema: z.object({
+        recipe: z.object({
+          name: z.string(),
+          ingredients: z.array(
+            z.object({
+              name: z.string(),
+              amount: z.string(),
+            }),
+          ),
+          steps: z.array(z.string()),
+        }),
+      }),
+    }),
+    prompt: query,
   });
 
-  console.log(text);
+  console.log(JSON.stringify(result.output.recipe, null, 2));
 }
 
 main(query)
